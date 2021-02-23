c3a03b3ad7e49a1ca8b8cec92a88b310955191ce
Chicco94 <enrico.cominato@gmail.com>
2021-02-23 10:57:04 +0100

    FIRST COMMIT

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+#%% Load the dataset and first exploratory analisys
+iris = pd.read('iris.csv', names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+print(iris.head())
